e706b1f2bb5ecd1098ac46bbc8978c3cef012283
Jonathan Selander <jonathan@mondaycreative.se>
2014-09-04 18:10:22 +0200

    result controller ajax thing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Loop54 Search Engine Implementation
+
+This implementation uses the basic Loop54 PHP library to replace the ordinary
+search results with their results.
+
+http://loop54.se/
---
+++ src/code/Loop54/controllers/ResultController.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Class Made_Loop54_AjaxController
+ */
+class Made_Loop54_ResultController extends Mage_Core_Controller_Front_Action
+{
+
+    public function ajaxAction()
+    {
+        $query = Mage::helper('catalogsearch')->getQuery();
+        /* @var $query Mage_CatalogSearch_Model_Query */
+
+        $query->setStoreId(Mage::app()->getStore()->getId());
+
+        if ($query->getQueryText() != '') {
+            if (Mage::helper('catalogsearch')->isMinQueryLength()) {
+                $query->setId(0)
+                    ->setIsActive(1)
+                    ->setIsProcessed(1);
+            }
+            else {
+                if ($query->getId()) {
+                    $query->setPopularity($query->getPopularity()+1);
+                }
+                else {
+                    $query->setPopularity(1);
+                }
+
+                if ($query->getRedirect()){
+                    $query->save();
+                    $this->getResponse()->setRedirect($query->getRedirect());
+                    return;
+                }
+                else {
+                    $query->prepare();
+                }
+            }
+
+            Mage::helper('catalogsearch')->checkNotes();
+
+            if (!Mage::helper('catalogsearch')->isMinQueryLength()) {
+                $query->save();
+            }
+        }
+
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
